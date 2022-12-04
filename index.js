1d23651afbf4478f2f5ae127f3197b46fb6daa79
GibadullinTagir <Gibadullin.t.r@gmail.com>
2022-12-04 15:05:55 +0300

    animation

---
+++ index.js
@@ -0,0 +1,19 @@
+function menuview() {
+    document.querySelector(".pop-up").style.display="block";
+};
+
+function menuHiden() {
+    document.querySelector(".pop-up").style.display="none";
+};
+
+// setTimeout(menuHiden2(), 1100);
+
+// function menuHiden2() {
+//     document.querySelector(".pop-up").style.display="none"
+// };
+
+
+// document.querySelector(".header__btn").onclick = function() {
+//     menuHiden();
+//     menuHiden2();
+// };
